7a4875543e322d578c088142afd17a301730b1a6
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2025-11-28 13:22:53 +0400

    feat: добавить в класс DemoOrderRepository метод save и findAll

--- src/main/java/com/github/goodcompanion/demo/DemoOrderRepository.java
+++ src/main/java/com/github/goodcompanion/demo/DemoOrderRepository.java
@@ -1,4 +1,20 @@
 package com.github.goodcompanion.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class DemoOrderRepository {
+    private final List<DemoOrder> orders = new ArrayList<>();
+    private final AtomicLong idCounter = new AtomicLong(1);
+
+    public DemoOrder save(DemoOrder order) {
+        order.setId(idCounter.getAndIncrement());
+        orders.add(order);
+        return order;
+    }
+
+    public List<DemoOrder> findAll() {
+        return new ArrayList<>(orders);
+    }
 }
