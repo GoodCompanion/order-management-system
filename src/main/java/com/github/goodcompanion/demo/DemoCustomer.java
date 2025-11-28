38bec4a824d8814b0ac88cd7a72929507e5a56e8
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2025-11-28 13:09:55 +0400

    feat: добавить поля, конструкторы, геттеры и сеттеры в DemoCustomer

--- src/main/java/com/github/goodcompanion/demo/DemoCustomer.java
+++ src/main/java/com/github/goodcompanion/demo/DemoCustomer.java
@@ -1,4 +1,40 @@
 package com.github.goodcompanion.demo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "demo_customer")
 public class DemoCustomer {
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+    public DemoCustomer() {
+    }
+
+    public DemoCustomer(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
