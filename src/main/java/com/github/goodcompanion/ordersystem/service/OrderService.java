cc3c67f1af995d1b96364b39841232f26a9057aa
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2025-11-30 21:47:04 +0400

    Добавить сервисный слой OrderService

--- src/main/java/com/github/goodcompanion/ordersystem/service/OrderService.java
+++ src/main/java/com/github/goodcompanion/ordersystem/service/OrderService.java
@@ -8,6 +8,9 @@ import com.github.goodcompanion.ordersystem.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class OrderService {
 
@@ -16,6 +19,14 @@ public class OrderService {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> getOrderById(Long id) {
+        return orderRepository.findById(id);
+    }
 
     public Order createOrder(CreateOrderRequest request) {
         Customer customer = customerRepository.findById(request.getCustomerId()).orElseThrow(() -> new RuntimeException("Клиент не найден"));
@@ -26,5 +37,30 @@ public class OrderService {
         order.setCustomer(customer);
 
         return orderRepository.save(order);
+    }
+
+    public Order updateOrder(Long id, Order orderDetails) {
+        Order order = orderRepository.findById(id).orElseThrow(() -> new RuntimeException("Заказ с ID " + id + " не найден"));
+
+        order.setDescription(orderDetails.getDescription());
+        order.setPrice(orderDetails.getPrice());
+        order.setStatus(orderDetails.getStatus());
+
+        if (orderDetails.getCustomer() != null) {
+            order.setCustomer(orderDetails.getCustomer());
+        }
+        return orderRepository.save(order);
+    }
+
+    public boolean deleteOrder(Long id) {
+        if (orderRepository.existsById(id)) {
+            orderRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Order> gerOrderByStatus(String status) {
+        return orderRepository.findByStatus(status);
     }
 }
